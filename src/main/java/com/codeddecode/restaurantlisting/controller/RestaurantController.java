fd8a2f86f46189db1d1216d32e4ac4f620ffe679
githubrepositoryuser <github.repository.user@gmail.com>
2025-03-28 18:51:21 +0530

    dummy commit for jenkins build

--- src/main/java/com/codeddecode/restaurantlisting/controller/RestaurantController.java
+++ src/main/java/com/codeddecode/restaurantlisting/controller/RestaurantController.java
@@ -24,6 +24,7 @@ public class RestaurantController {
         return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
     }
 
+
     @PostMapping("/addRestaurant")
     public ResponseEntity<RestaurantDTO> saveRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
         RestaurantDTO restaurantAdded = restaurantService.addRestaurantInDB(restaurantDTO);
